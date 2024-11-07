4f2c12c5a38b5aa66642f17e61a204f4316afcf2
sendog6913 <166747938+sendog6913@users.noreply.github.com>
2024-11-07 13:38:58 -0800

    updated top nav to be responsive. see #6

--- docs/Scripts.js
+++ docs/Scripts.js
@@ -2,3 +2,12 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+function ResponsiveNav() {
+  var x = document.getElementById("Links");
+  if (x.className === "RightTopLinks") {
+    x.className += " responsive";
+  } else {
+    x.className = "RightTopLinks";
+  }
+}
