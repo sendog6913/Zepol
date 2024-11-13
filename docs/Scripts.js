03043e992256831d0bfbdceda81e7cc5c7c9c8fd
sendog6913 <166747938+sendog6913@users.noreply.github.com>
2024-11-13 15:28:59 -0800

    adjusted font awesome

--- docs/Scripts.js
+++ docs/Scripts.js
@@ -11,3 +11,18 @@ function ResponsiveNav() {
     x.className = "RightTopLinks";
   }
 }
+
+function loadHTMLFromFunction() {
+  const htmlCode = `
+    <div>
+      <h2>Hello from JavaScript!</h2>
+      <p>This content was loaded dynamically.</p>
+    </div>
+  `;
+
+  // Get the target element where you want to insert the HTML
+  const targetElement = document.getElementById('target'); 
+
+  // Insert the HTML code into the target element
+  targetElement.innerHTML = htmlCode; 
+}
