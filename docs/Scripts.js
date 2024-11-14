8835d05c3059095ecdd1ed8d826dd03e3b9c7d9c
sendog6913 <166747938+sendog6913@users.noreply.github.com>
2024-11-13 16:40:32 -0800

    dynamic navbar using javascript, resources listing. see #6 and #9

--- docs/Scripts.js
+++ docs/Scripts.js
@@ -1,28 +1,37 @@
-// Simple Scripts to practice 
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
 
-function ResponsiveNav() {
-  var x = document.getElementById("Links");
-  if (x.className === "RightTopLinks") {
-    x.className += " responsive";
-  } else {
-    x.className = "RightTopLinks";
-  }
-}
-
-function loadHTMLFromFunction() {
+function LoadMainNavigation() {
   const htmlCode = `
-    <div>
-      <h2>Hello from JavaScript!</h2>
-      <p>This content was loaded dynamically.</p>
-    </div>
+      <div class="NavBar fa-solid fa-1x">
+        <div id="Logo"></div>
+        <div class="RightTopLinks" id="Links">
+          <a href="https://sendog6913.github.io/Zepol" target="_parent" class="active">Home</a>
+          <a href="#" target="_self">Contact</a>
+          <a href="#" target="_self">My CV</a>
+          <a href="resources.html" target="_self">Resources</a>
+          <a href="javascript:void(0);" class="icon" onclick="ResponsiveNav()">
+            <i class="fa fa-bars"></i>
+          </a>
+        </div>
+      </div>
   `;
 
   // Get the target element where you want to insert the HTML
-  const targetElement = document.getElementById('target'); 
+  const targetElement = document.getElementById('NavBarTarget'); 
 
   // Insert the HTML code into the target element
   targetElement.innerHTML = htmlCode; 
+}
+
+  function ResponsiveNav() {
+    var x = document.getElementById("Links");
+    if (x.className === "RightTopLinks") {
+      x.className += " responsive";
+    } else {
+      x.className = "RightTopLinks";
+    }
+  }
+
+// Simple Scripts to practice 
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
